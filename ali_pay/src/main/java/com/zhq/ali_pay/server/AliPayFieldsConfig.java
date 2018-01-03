b5282d0ddc1d37c5e310c365979bbc77781ed90f
ZHQ <sfgjys@sina.com>
2018-01-03 15:29:12 +0800

    封装获取最后支付所需要的订单字符串方法

--- ali_pay/src/main/java/com/zhq/ali_pay/server/AliPayFieldsConfig.java
+++ ali_pay/src/main/java/com/zhq/ali_pay/server/AliPayFieldsConfig.java
@@ -1,5 +1,7 @@
 package com.zhq.ali_pay.server;
 
+import java.util.concurrent.BlockingDeque;
+
 /**
  * 类的描述: 支付宝支付功能模块的字段配置
  */
@@ -12,4 +14,12 @@ public class AliPayFieldsConfig {
      * 变量的描述: 签名算法类型
      */
     public static final String SIGN_TYPE = "RSA2";
+    /**
+     * 变量的描述: RSA2签名算法的私钥
+     */
+    public static final String RSA2_PRIVATE = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCmA592KmPH8lBzEkeon8uSv/NYZwOC5u1Tmoq52xmEIfM3ETUiA2CFpz4kV2nizX3acfIYBkFojJEKhrwTwNr/ziQhcWg+YxKCmPQYecu//5ixnHQSN7DEuv3QHW9DpBWu0ia6IF5QLnO0/OStDr4a7sT/QEU5zsleNE/LjV1PTfsewjuIoJndpjt/eu3AY+t4pg+QNjqVioUviv7/4FYx+mENElMrfNOEoB86hCm9nJpCPu/WGOUkKSTRMWxC9v/AebRa0InyrW+XoGOez4Et0xlkJHuvMESbqn4oNFlys03SpVJAFcwS4DrATf49Me0Jfe7Nt6jniVZeOgq7AHCzAgMBAAECggEAVgZ6kxjUK7/8wCQwzchADQM6bgyzXRmc0T27tSM7zaRqC5b3GXD2i0tmgB5r2ALzvEZfVOOo8Swl3HaE7xd4ZCXFwy6o/esIpHPtyOEUG6Gu1hpkayxAhTEAZN9b0IkuUzDCsKAlPxdTDe4d8S86g2pG2p3LQXFOF2q0Dp5qQ7Uk9LBPl5aWQORN/TskC1eYgiqQAtKpT4mSwi/0+gL+a+IRyeq/kS+o3FtGUvFkA9wL2dabLgrugHCZ4KVI6CU2a/EFGJGxTw7BK91mxoxyrCtvGxbSz/eEAXbZuesC7VXXnzeG6f6Asuo1SEo2OH3T6QmKycT5GHBifs6huml/wQKBgQDvBTJ4HEebejNLQd4y6pVxN2MzJLNsQ8boVL/zVVUuz86UzuNntznEUSSmMfnEWkitSO/NNJOFg5IJcgPllsH9npZKTZY6r5mxQo6CcyOl7mK5JKzxGngbyleNNOITj1VNpJZ81Mbz2doFHflMhlHAmpOkHLhZP8cwoRnEBodi5QKBgQCxzr38adQYUvdRrxmdS+tGYNanu2WGuCRyfcfLmyprW2m/+H7Qd7SKUPDea+1tP6BGi2mh15HSmxvxsNa70mUkvU0+cDMPB7w8xlXyBszGQa9ALRbTcKOBOB/l6JyIpQE2W2omwsWByxBmV7xfMe4hpaHVyv24unExvu1AyX7TtwKBgBhvg5Vb7sYqXAwZk8nIybZlXzLfUaiD1VXzNByAYNEEVa5I6F62EKXNVd3Xxh0Yzc1JY5qrAVno9YkZquRR4Us4Z0z5WZ5uTVwd3cvgUYipMlaTNUsA1b6hlQnOmj49rQ7fALOKXTyYzP0w70evg7QZGXbxqLcXGhoSnju4HycxAoGBAIwb90VBNyn7pnstWSfDvR92C2+ckUH5NG8rghaErIZwQAJPk849pXwgj80UisC9lFRCYvhu369vHjPzPyI9hgnPSWDcFL/RPpk9SFgw14eaFrihTw1KAu0+BpSqGT7rlur4Wcs1S2EdXEzo8bypoZXN6JqJHClGcQ/b/SRCIrg9AoGBAJaaMkiR0OPeWRPIw7p335Heyw2Fuj6pXsxDunhVGdUife7vDHKH0IOyb2XugBg44UkxwmmQcxDDwTCMM0yUD250WEjtQm80TEkTdFwA2t5+06dC5c9c5lPjeCw24M1KQaPe/1FhtlVd0CqgnM5x7PqmH7gGVO3HjutwyEq7b5p4";
+    /**
+     * 变量的描述: 支付结果通知回调Url
+     */
+    public static final String PAY_RESULT_NOTIFY_CALLBACK_URL = "";
 }
--- ali_pay/src/main/java/com/zhq/ali_pay/server/OrderFormInfoTool.java
+++ ali_pay/src/main/java/com/zhq/ali_pay/server/OrderFormInfoTool.java
@@ -1,6 +1,12 @@
 package com.zhq.ali_pay.server;
 
+import com.zhq.ali_pay.server.sign.SignUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,7 +19,7 @@ public class OrderFormInfoTool {
      *
      * @param currentTimeMillis 发送支付请求的时间，格式"yyyy-MM-dd HH:mm:ss"
      */
-    public static Map<String, String> orderFormPublicInfoShiftMap(String currentTimeMillis) {
+    public static Map<String, String> getOrderFormPublicInfoMap(String currentTimeMillis) {
         Map<String, String> orderFormInfo = new HashMap<>();
         // 接口名称
         orderFormInfo.put("method", "alipay.trade.app.pay");// 服务端
@@ -27,17 +33,90 @@ public class OrderFormInfoTool {
         orderFormInfo.put("sign_type", AliPayFieldsConfig.SIGN_TYPE);// 服务端
         // 支付宝分配给开发者的应用ID
         orderFormInfo.put("app_id", AliPayFieldsConfig.APP_ID);// 服务端
+        // 支付宝服务器主动通知商户服务器里指定的页面http/https路径。建议商户使用https
+        orderFormInfo.put("notify_url", AliPayFieldsConfig.PAY_RESULT_NOTIFY_CALLBACK_URL);// 服务端
+
         // 发送请求的时间，格式"yyyy-MM-dd HH:mm:ss"
         orderFormInfo.put("timestamp", currentTimeMillis);// 客户端
 
         // 业务请求参数的集合，最大长度不限，除公共参数外所有请求参数都必须放在这个参数中传递，具体参照各产品快速接入文档
         orderFormInfo.put("biz_content", "");// TODO????????????
-        //
-        orderFormInfo.put("notify_url", "");// TODO????????????
 
 
-        //
-        orderFormInfo.put("sign", "");// TODO????????????
+        // TODO orderFormInfo.put("sign", ""); 签名的值不在这里赋值
         return orderFormInfo;
+    }
+
+    /**
+     * 方法描述: 将拼接过的所有键值对通过 "&" 来拼接，进而获取除了“sign”参数的所有支付请求参数拼接完成的字符串
+     */
+    public static String jointAllPayParameterExceptSign(Map<String, String> map) {
+        // 获取所有所有键值对的Key
+        List<String> keys = new ArrayList<>(map.keySet());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        // 这里获取了除了最后一对的所有键值对
+        for (int i = 0; i < keys.size() - 1; i++) {
+            // 从集合中获取Key
+            String key = keys.get(i);
+            // 获取Key对应的Value
+            String value = map.get(key);
+            // 将Key和Value先进行拼接，在把拼接结果作为整体在拼接
+            stringBuilder.append(jointKeyValue(key, value, true));
+            stringBuilder.append("&");
+        }
+
+        // 获取最后一对键值对
+        String tailKey = keys.get(keys.size() - 1);
+        String tailValue = map.get(tailKey);
+        // 最后拼接
+        stringBuilder.append(jointKeyValue(tailKey, tailValue, true));
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 方法描述: 以"="来拼接键值对，并且根据参数来确定是否对value进行编码
+     */
+    private static String jointKeyValue(String key, String value, boolean isEncode) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(key);// 先添加Key
+        stringBuilder.append("=");// 在添加"="
+        if (isEncode) {
+            try {
+                // 对value进行编码后在拼接
+                stringBuilder.append(URLEncoder.encode(value, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                stringBuilder.append(value);
+            }
+        } else {
+            stringBuilder.append(value);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 方法描述: 给已经拼接的所有支付参数(除了sign)进行加密，加密方法是参数三，加密密钥是参数二
+     *
+     * @return 获取的是“sign=加密结果”
+     */
+    public static String encryptionAllPayParameterExceptSign(String allPayParameterExceptSign, String privateKey, boolean isRSA2) {
+        String signResult = SignUtils.sign(allPayParameterExceptSign, privateKey, isRSA2);
+        String encodedSignResult = "";
+        try {
+            // 给加密结果进行编码
+            encodedSignResult = URLEncoder.encode(signResult, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "sign=" + encodedSignResult;
+    }
+
+    /**
+     * 方法描述: 获取最终支付用的订单信息String
+     */
+    public static String getUltimatelyPayInfoString(Map<String, String> map) {
+        String unSignPayInfo = jointAllPayParameterExceptSign(map);
+        String signPayInfo = encryptionAllPayParameterExceptSign(unSignPayInfo, AliPayFieldsConfig.RSA2_PRIVATE, true);
+        return unSignPayInfo + "&" + signPayInfo;
     }
 }
