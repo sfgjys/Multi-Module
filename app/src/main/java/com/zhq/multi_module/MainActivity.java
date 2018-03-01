aea15ccc8a132ca91806d9a1feefa83943d34e45
ZHQ <sfgjys@sina.com>
2018-03-01 17:04:27 +0800

    使用自定义控件模块

--- app/src/main/java/com/zhq/multi_module/MainActivity.java
+++ app/src/main/java/com/zhq/multi_module/MainActivity.java
@@ -1,8 +1,19 @@
 package com.zhq.multi_module;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.zhq.baselibrary.base.PermissionActivity;
+
+import com.zhq.custom_view.banner.CoverFlow;
+import com.zhq.custom_view.banner.PageItemClickListener;
+import com.zhq.custom_view.banner.PagerContainer;
 
 public class MainActivity extends PermissionActivity {
 
@@ -10,5 +21,58 @@ public class MainActivity extends PermissionActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PagerContainer container = (PagerContainer) findViewById(R.id.pager_container);
+        ViewPager pager = container.getViewPager();
+        pager.setAdapter(new MyPagerAdapter());
+        pager.setClipChildren(false);
+        //
+        pager.setOffscreenPageLimit(15);
+
+        container.setPageItemClickListener(new PageItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        new CoverFlow.Builder()
+                .with(pager)
+                .scale(0.3f)
+                .pagerMargin(getResources().getDimensionPixelSize(R.dimen.pager_margin))
+                .spaceSize(0f)
+                .build();
     }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_cover, null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.image_cover);
+            imageView.setImageDrawable(getResources().getDrawable(covers[position]));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return covers.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return (view == object);
+        }
+    }
+
+    public static int[] covers = {R.mipmap.ic_cover_1, R.mipmap.ic_cover_2, R.mipmap.ic_cover_3, R.mipmap.ic_cover_4,
+            R.mipmap.ic_cover_5, R.mipmap.ic_cover_6, R.mipmap.ic_cover_7, R.mipmap.ic_cover_8};
 }
